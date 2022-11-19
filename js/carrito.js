7d62cac5164abc12977dc33e34d88b7702b02106
Camilo Rueda <camilojrueda@gmail.com>
2022-11-19 15:18:39 -0500

    error pendiente en calcularTotal

--- js/carrito.js
+++ js/carrito.js
@@ -4,6 +4,7 @@ let articuloCarrito = document.getElementById("articuloCarrito")
 
 let carrito = JSON.parse(localStorage.getItem("info")) || []
 
+//suma la cantidad  de cada item y lo plasma 
 let calculo = ()=>{
     let cantidadCarrito = document.getElementById("cantidadCarrito");
 
@@ -13,41 +14,52 @@ let calculo = ()=>{
 
 calculo()
 
-console.log(carrito)
-
 let generarCarrito= ()=>{
-    if(carrito.length !== 0){
-        return (articuloCarrito.innerHTML= carrito.map((articulo)=>{
-        let {id, item} = articulo
+    fetch("../productos.json")
+    .then (function(resp){
+        return resp.json()
+    })
+    .then(function(productos){
+            //creando tarjetas para cada producto en carrito
+        if(carrito.length !== 0){
+            return (       
+                articuloCarrito.innerHTML= carrito.map((articulo)=>{
+            let {id, item} = articulo
+    
+            let buscar = productos.find((x)=>x.id === id) || []
+    
+            let buscarProducto = carrito.find((p)=>p.id === id) 
+            //calcula el precio de cada producto
+            let sumaProducto= buscar.precio*buscarProducto.item
+                return `
+    
+                <div class="col-3 mb-2 ">
+            <img class="w-50" src="${buscar.img}" alt="art image">
+          </div>
+          <div class="col-5 mb-2 ">
+            <h6 class="">${buscar.nombre}</h6>
+            <p class="pl-1 mb-0">$ ${buscar.precio}</p>
+            <div class="pl-1 mb-0">
+            <button type="button" class="btn btn-danger mb-2 mx-auto" onclick="eliminarProducto(${id})">Eliminar</button>
+            </div>
+          </div>
+          <div class="col-2 d-flex">
+          <button onclick="reducirCantidad(${id})" class="btn value-button">-</button>
+          <div id=${id} id="cantidad" class="cantidad">${buscarProducto === undefined ? 0 : buscarProducto.item}</div>                      
+                <button onclick="aumentarCantidad(${id})"  class="btn value-button">+</i>
+          </div>
+          <div class="col-2">
+            <p id="cartItem1Price">$${sumaProducto}</p>
+          </div>
+          <hr class="division">
+    
+                `
+            
+        }).join(" "));
+        
+    }
 
-        let buscar = productos.find((x)=>x.id === id) || []
-
-        let buscarProducto = carrito.find((p)=>p.id === id) 
-            return `
-
-            <div class="col-3 mb-2 ">
-        <img class="w-50" src="${buscar.img}" alt="art image">
-      </div>
-      <div class="col-5 mb-2 ">
-        <h6 class="">${buscar.nombre}</h6>
-        <p class="pl-1 mb-0">$ ${buscar.precio}</p>
-        <div class="pl-1 mb-0">
-        <button type="button" class="btn btn-danger">Eliminar</button>
-        </div>
-      </div>
-      <div class="col-2 d-flex">
-      <button onclick="reducirCantidad(${id})" class="btn value-button">-</button>
-      <div id=${id} class="cantidad">${buscarProducto === undefined ? 0 : buscarProducto.item}</div>                      
-            <button onclick="aumentarCantidad(${id})"  class="btn value-button">+</i>
-      </div>
-      <div class="col-2">
-        <p id="cartItem1Price">$66</p>
-      </div>
-      <hr class="division">
-
-            `
-    }).join(" "));
-    } else {
+    else {
 
         articuloCarrito.innerHTML= `
 
@@ -57,11 +69,147 @@ let generarCarrito= ()=>{
     estadoCarrito.innerHTML = `
         <h2>Tu carrito está vacio</h2>
         <a href="catalogo.html">
-            <button class="btn btn-lg btn-primary">Volver al Catálogo</button>
+            <button class="btn btn-lg btn-primary d-flex justify-content-center mt-3">Volver al Catálogo</button>
         </a>
 
         `
     }
-}
+})}
+  
 
 generarCarrito()
+
+let aumentarCantidad = (id)=>{
+
+    let productoSeleccionado = id
+
+    let buscarProducto = carrito.find((p)=>p.id === productoSeleccionado);
+
+        if (buscarProducto === undefined){
+        carrito.push ({
+            id: productoSeleccionado,
+            item: 1,
+        });
+
+        }else {buscarProducto.item +=1;}
+
+        calculo()
+    
+    localStorage.setItem("info", JSON.stringify(carrito))
+        
+    calcularTotal ()
+
+    generarCarrito()
+
+
+    Toastify({
+
+        text: "Agregaste un Producto!",
+        
+        duration: 3000
+        
+        }).showToast();
+
+}
+
+let reducirCantidad = (id) =>{
+    let productoSeleccionado = id
+
+    let buscarProducto = carrito.find((p)=>p.id === productoSeleccionado);
+
+    if(buscarProducto === undefined) return;
+    else if (buscarProducto.item === 0) return;
+    else {buscarProducto.item -=1;}
+
+    calculo()
+
+    carrito = carrito.filter((p)=>p.item !== 0)
+
+    localStorage.setItem("info", JSON.stringify(carrito))
+
+    calcularTotal ()
+
+    generarCarrito()
+
+    Toastify({
+
+        text: "Eliminaste un Producto!",
+        
+        duration: 3000
+        
+        }).showToast();
+
+
+}
+
+
+let eliminarProducto = (id) =>{
+
+    let productoSeleccionado = id
+
+    carrito = carrito.filter((x)=>x.id !== productoSeleccionado)
+
+    localStorage.setItem("info", JSON.stringify(carrito))
+
+    calculo()
+
+    generarCarrito()
+
+   
+}
+//plasma el precio total de la compra
+let calcularTotal= ()=>{
+    if  (carrito.length !== 0){
+    fetch("../productos.json")
+        .then (function(resp){
+            return resp.json()
+        })
+        .then(function(productos){
+        console.log(productos)
+        }}
+    else return
+}
+
+//elmina la totalidad del carrito
+let borrarCarrito = ()=>{
+    swal({
+        title: "Estás seguro?",
+        text: "Una vez elimines los productos tendras que volver a seleccionar tus productos",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        
+    
+        if (willDelete) {
+
+        carrito = []
+
+        calculo()
+    
+        generarCarrito()
+    
+        localStorage.setItem("info", JSON.stringify(carrito))
+        
+          swal("Eliminaste los productos", {
+            icon: "success",
+          });
+        } else {
+          swal("Continua comprando");
+        }
+      });
+
+
+    
+
+
+}
+
+//confirma al cliente que esta listo para pasar a la pasarela de pago
+let confirmarPago = ()=>{
+
+    swal("Todo listo!", "Vamos a avanzar con el proceso de pago", "success")
+}
+
+calcularTotal ()
--- js/main.js
+++ js/main.js
@@ -1,80 +1,108 @@
+//tomando id para inyectar html
 let catalogo = document.getElementById('catalogoArticulos')
 
+//tomando info previa de local storage
 let carrito = JSON.parse(localStorage.getItem("info")) || []
 
 
 
 let crearCatalogo = () => {
-    return (catalogo.innerHTML = productos.map(({id, nombre, descripcion, precio, img,})=>{
+    //creando tarjetas para cada producto
+    fetch("../productos.json")
+    .then (function(resp){
+        return resp.json()
+    })
+    .then(function(productos){
+        return (catalogo.innerHTML = productos.map(({id, nombre, descripcion, precio, img,})=>{
         
 
-        let buscarProducto = carrito.find((p)=>p.id === id) 
+            let buscarProducto = carrito.find((p)=>p.id === id) 
+            
+            return `
+                        <div  id ="idProducto-${id}" class="card" style="width: 16rem;">
+                        <img class="card-img-top" src="${img}" alt="Card image cap">
+                        <div class="card-body">
+                          <h5 class="card-title">${nombre}</h5>
+                          <p class="card-text">${descripcion}</p>
+                        </div>
+                        <div class="card-body">
+                        <p>$${precio}</p>
+                        <div class="d-flex">
+                          <button onclick="reducirCantidad(${id})" class="btn value-button">-</button>
+                          <div id=${id} class="cantidad">${buscarProducto === undefined ? 0 : buscarProducto.item}</div>                      
+                                <button onclick="aumentarCantidad(${id})"  class="btn value-button">+</i>
+                        </div>
+                        </div>
+                      </div>
+    
+            `
+        }).join(""))
         
-        return `
-                    <div  id ="idProducto-${id}" class="card" style="width: 16rem;">
-                    <img class="card-img-top" src="${img}" alt="Card image cap">
-                    <div class="card-body">
-                      <h5 class="card-title">${nombre}</h5>
-                      <p class="card-text">${descripcion}</p>
-                    </div>
-                    <div class="card-body">
-                    <p>$${precio}</p>
-                    <div class="d-flex">
-                      <button onclick="reducirCantidad(${id})" class="btn value-button">-</button>
-                      <div id=${id} class="cantidad">${buscarProducto === undefined ? 0 : buscarProducto.item}</div>                      
-                            <button onclick="aumentarCantidad(${id})"  class="btn value-button">+</i>
-                    </div>
-                    </div>
-                  </div>
-
-        `
-    }).join(""))
+    })
+    
     
 }
    
 crearCatalogo()
 
 let aumentarCantidad = (id)=>{
-
+    //se identifica producto clickeado
     let productoSeleccionado = id
 
     let buscarProducto = carrito.find((p)=>p.id === productoSeleccionado);
-
+        //en caso de que el producto no este en el carrito, se agrega
         if (buscarProducto === undefined){
         carrito.push ({
             id: productoSeleccionado,
             item: 1,
         });
-
+        //en caso de que ya este agregado, se aumenta la cantidad
         }else {buscarProducto.item +=1;}
 
-    calculo()
-    
-    localStorage.setItem("info", JSON.stringify(carrito))
 
+    //se calcula nuevamente la cantidad de items en total en el carrito, se guarda y se renderiza nuevamente    
+    calculo()
+    localStorage.setItem("info", JSON.stringify(carrito))
     crearCatalogo()
+
+    Toastify({
+
+        text: "Agregaste un Producto!",
+        
+        duration: 3000
+        
+        }).showToast();
 
 }
 
 let reducirCantidad = (id) =>{
+    //se identifica producto clickeado
     let productoSeleccionado = id
 
     let buscarProducto = carrito.find((p)=>p.id === productoSeleccionado);
-
+    //evita que la cantidad de producto sea menor a 0
     if(buscarProducto === undefined) return;
     else if (buscarProducto.item === 0) return;
     else {buscarProducto.item -=1;}
 
     calculo()
-
+    // se encuentra el producto cuyo valor llego a 0 y se elimina del carrito
     carrito = carrito.filter((p)=>p.item !== 0)
 
     localStorage.setItem("info", JSON.stringify(carrito))
 
     crearCatalogo()
 
-}
+    Toastify({
 
+        text: "Eliminaste un Producto!",
+        
+        duration: 3000
+        
+        }).showToast();
+
+}
+//actualiza el nuemro de items en el carro
 let calculo = ()=>{
     let cantidadCarrito = document.getElementById("cantidadCarrito");
 
